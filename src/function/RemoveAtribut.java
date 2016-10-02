f443273bde7a253442824056ba5c896cbe8f87f3
ginanjar busiri <gbusiri@gmail.com>
2016-10-02 20:02:08 +0700

    function remove attribute

--- src/function/RemoveAtribut.java
+++ src/function/RemoveAtribut.java
@@ -1,5 +1,13 @@
 package function;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import weka.core.Instance;
+import weka.core.Instances;
+import weka.filters.Filter;
+import weka.filters.unsupervised.attribute.Remove;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -12,4 +20,20 @@ package function;
  */
 public class RemoveAtribut {
     
+    public static Instances removeAttribut(Instances inst) throws IOException, Exception {
+        Instances ret = null;
+        Remove rem = new Remove();
+        
+        if (inst.classIndex() < 0) {
+            ret = inst;
+        } else {
+            rem.setAttributeIndices(""+(inst.classIndex()+1));
+            rem.setInvertSelection(false);
+            rem.setInputFormat(inst);
+            ret = Filter.useFilter(inst, rem);
+        }
+            
+        return ret;
+    }
+        
 }
