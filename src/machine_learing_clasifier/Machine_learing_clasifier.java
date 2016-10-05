df93359d901a7fb6ab6f130fac8423f2d0b78d44
azwaradli <azwar.adli@gmail.com>
2016-10-05 17:57:17 +0700

    add classify instance

--- src/machine_learing_clasifier/Machine_learing_clasifier.java
+++ src/machine_learing_clasifier/Machine_learing_clasifier.java
@@ -17,15 +17,19 @@ import weka.core.Instances;
  */
 public class Machine_learing_clasifier {
     public static MyC45 myc45 = new MyC45();
+    public static MyID3 myid3 = new MyID3();
+    
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) throws Exception {
         // TODO code application logic here
-        Instances data = new Instances(LoadData.getData("D:\\Documents\\ML\\machine_learing_clasifier\\weather.numeric.arff"));
-        myc45.buildClassifier(data);
-        
+        Instances data = new Instances(LoadData.getData("E:\\machine_learning_classifier\\weather.nominal.arff"));
+        myid3.buildClassifier(data);
+        for(int i = 0; i < data.size(); i++){
+            double sesuatu = myid3.classifyInstance(data.get(i));
+            System.out.println("output = "+sesuatu);
+        }
     }
-
     
 }
